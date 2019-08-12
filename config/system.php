f306ce1655e6e33f5754e5158d21bf1adef7dc07
Fábio Valentim <maitabom@gmail.com>
2019-08-12 13:24:02 -0300

    Criação do arquivo de sistema e paginação

---
+++ config/system.php
@@ -0,0 +1,28 @@
+<?php
+
+return[
+    'System' => [
+        'name'=> 'Frotas | Prefeitura Municipal de Coqueiral',
+        'shortName' => 'Frotas da Prefeitura de Coqueiral',
+        'version' => '1.0',
+        'fullVersion' => '1.0.0',
+        'yearRelease' => '2019',
+        'yearCreation' => '2019'
+    ],
+    'Author' => [
+        'name' => 'Prefeitura Municipal de Coqueiral',
+        'company' => 'Prefeitura Municipal de Coqueiral',
+        'local' => 'Coqueiral - MG',
+        'site' => 'coqueiral.mg.gov.br',
+        'email' => 'ti@coqueiral.mg.gov.br',
+        'developer' => [
+            'name' => 'Fábio Valentim',
+            'site' => 'www.baudovalentim.net',
+            'email' => 'valentim@baudovalentim.net'
+        ]
+    ],
+    'Host' => [
+        'developer' => 'localhost',
+        'release' => 'coqueiral.mg.gov.br'
+    ]
+];
