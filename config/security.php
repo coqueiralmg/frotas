083ff9566526c367aa1445a8be46e974ce256b23
Fábio Valentim <maitabom@gmail.com>
2019-08-12 13:24:43 -0300

    Alteração das configurações de segurança

--- config/security.php
+++ config/security.php
@@ -10,5 +10,12 @@ return [
      */
     'Security' => [
         'salt' => env('SECURITY_SALT', 'c7dc482249a36e0107dfba84d16f875ac1d14d91a1f29dd93fd2602036dc66f9'),
+        'login' => [
+            'attemps' => [
+                'max' => 5,
+                'warning' => 3
+            ],
+            'access' => 'public'
+        ]
     ],
 ];
