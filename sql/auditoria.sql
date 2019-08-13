cb32127033d6ea24498d90ec70b4b8eb19439b1b
Fábio Valentim <maitabom@gmail.com>
2019-08-13 16:14:03 -0300

    Adição de funções de controle de acesso no banco de auditoria

---
+++ sql/auditoria.sql
@@ -0,0 +1,9 @@
+INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('1', 'Logon no sistema', 'Ocorre quando o usuário acessa o sistema com o login válido', '1');
+INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('2', 'Troca de senha', 'Ocorre quando é efetuado a troca de senha', '1');
+INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('3', 'Bloqueio de IP', 'Quando o administrador bloqueia o acesso ao sistema via IP', '1');
+INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('4', 'Suspensão da conta', 'Quando a conta é suspensa', '1');
+INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('5', 'Limpeza de cache e de sessão', 'Limpeza de cache e do sistema', '1');
+INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('6', 'Acesso suspeito', 'Quando o acesso ao sistema é suspeito', '1');
+INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('7', 'Acesso bloqueado', 'Quando o acesso ao sistema é bloqueado', '1');
+INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('8', 'Logoff no sistema', 'Quando o usuário sai do sistema', '1');
+INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('9', 'Impressão de documento', 'Quando é feito uma impressão de documento', '1');
