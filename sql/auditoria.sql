219176dcba78402fcf87c14d0d84321cfeafca7e
Fábio Valentim <maitabom@gmail.com>
2019-08-14 14:11:27 -0300

    Alteração da lista de atividades de auditoria, segundo a nova estrutura de tabela

--- sql/auditoria.sql
+++ sql/auditoria.sql
@@ -1,9 +1,13 @@
-INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('1', 'Logon no sistema', 'Ocorre quando o usuário acessa o sistema com o login válido', '1');
-INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('2', 'Troca de senha', 'Ocorre quando é efetuado a troca de senha', '1');
-INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('3', 'Bloqueio de IP', 'Quando o administrador bloqueia o acesso ao sistema via IP', '1');
-INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('4', 'Suspensão da conta', 'Quando a conta é suspensa', '1');
-INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('5', 'Limpeza de cache e de sessão', 'Limpeza de cache e do sistema', '1');
-INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('6', 'Acesso suspeito', 'Quando o acesso ao sistema é suspeito', '1');
-INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('7', 'Acesso bloqueado', 'Quando o acesso ao sistema é bloqueado', '1');
-INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('8', 'Logoff no sistema', 'Quando o usuário sai do sistema', '1');
-INSERT INTO `frotas`.`atividade` (`id`, `nome`, `descricao`, `habilitado`) VALUES ('9', 'Impressão de documento', 'Quando é feito uma impressão de documento', '1');
+/*
+-- Query: select * from atividade
+-- Date: 2019-08-14 14:08
+*/
+INSERT INTO `` (`id`,`nome`,`descricao`,`validar`,`ativo`) VALUES (1,'Logon no sistema','Ocorre quando o usuário acessa o sistema com o login válido',1,1);
+INSERT INTO `` (`id`,`nome`,`descricao`,`validar`,`ativo`) VALUES (2,'Troca de senha','Ocorre quando é efetuado a troca de senha',1,1);
+INSERT INTO `` (`id`,`nome`,`descricao`,`validar`,`ativo`) VALUES (3,'Bloqueio de IP','Quando o administrador bloqueia o acesso ao sistema via IP',0,1);
+INSERT INTO `` (`id`,`nome`,`descricao`,`validar`,`ativo`) VALUES (4,'Suspensão da conta','Quando a conta é suspensa',0,1);
+INSERT INTO `` (`id`,`nome`,`descricao`,`validar`,`ativo`) VALUES (5,'Limpeza de cache e de sessão','Limpeza de cache e do sistema',0,1);
+INSERT INTO `` (`id`,`nome`,`descricao`,`validar`,`ativo`) VALUES (6,'Acesso suspeito','Quando o acesso ao sistema é suspeito',0,1);
+INSERT INTO `` (`id`,`nome`,`descricao`,`validar`,`ativo`) VALUES (7,'Acesso bloqueado','Quando o acesso ao sistema é bloqueado',0,1);
+INSERT INTO `` (`id`,`nome`,`descricao`,`validar`,`ativo`) VALUES (8,'Logoff no sistema','Quando o usuário sai do sistema',0,1);
+INSERT INTO `` (`id`,`nome`,`descricao`,`validar`,`ativo`) VALUES (9,'Impressão de documento','Quando é feito uma impressão de documento',0,1);
