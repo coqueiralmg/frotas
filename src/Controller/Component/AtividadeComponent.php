4a8ff99f5174365176923b1d71cc17d059a25e6b
Fábio Valentim <maitabom@gmail.com>
2019-08-14 16:16:19 -0300

    Implantação da função que valida a atividade de auditoria

--- src/Controller/Component/AtividadeComponent.php
+++ src/Controller/Component/AtividadeComponent.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller\Component;
 
+use App\Error\AuditoriaException;
+use App\Model\Entity\Atividade;
+use Cake\ORM\TableRegistry;
+use Exception;
+
 /**
  * Classe que representa a atividade a ser registrada na auditoria
  */
@@ -16,4 +21,30 @@ class AtividadeComponent extends Component
     const SYSTEM_ACESSO_BLOQUEADO = 7;
     const SYSTEM_LOGOFF_SISTEMA = 8;
     const SYSTEM_IMPRESSAO_DOCUMENTO = 9;
+
+    /**
+     * Verifica se a atividade existe no banco de dados e retorna a mesma, com todas as informações requeridas.
+     * @throws AuditoriaException O usuário não tem a permissão de acessar a determinada tela do sistema.
+     * $return Atividade Atividade da auditoria do sistema.
+     */
+    public function validar(int $codigoAtividade)
+    {
+        try
+        {
+            $t_atividades = TableRegistry::get('Atividade');
+            $atividade = $t_atividades->get($codigoAtividade);
+
+            return $atividade;
+        }
+        catch(Exception $ae)
+        {
+            $message = [
+                'message' => 'Ocorreu um erro ao buscar uma atividade',
+                'atividade' => $codigoAtividade
+            ];
+
+            throw new AuditoriaException($message, null, $ae);
+        }
+
+    }
 }
